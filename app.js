cd14c3d3a2cdd6412eedb60d0ee17513ae443870
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-02-23 10:43:23 +0200

    added buy seat fucn and reworked choose seat fucntion

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let hall = document.getElementById("hall");
 
-let seats = document.getElementsByClassName("seat")
+let seats = document.getElementsByClassName("seat");
+let selectedSeatContainer = document.querySelector('.film-info__seat');
 
 
 const row = [8, 10, 12, 12, 12, 12, 12];    // array of hall
 let hallMapArr = [];                        // 3dimension array of hall
 let selectedSeatsArr = [];                  //  save all selected seat
+let totalCost = 0;
 
 
 function fillHall() {
@@ -24,7 +26,7 @@ function fillHall() {
                     row : index+1,
                     seat : i,
                     state : 'free',
-                    price : '5'
+                    price : 5
                 }
             );
         }
@@ -38,39 +40,49 @@ function fillHall() {
 
 function addSeatsToBuy() {                  // сделать так чтобы функция не срабатывала для купленных мест
     for (let seat of seats) {
-        seat.addEventListener("click", function() {
-            let hallRowNum = seat.getAttribute('data-row');
-            let hallSeatNum = seat.getAttribute('data-seat');
-    
-            seat.classList.toggle('hall-info__legend-color_selected');
-            seat.classList.toggle('hall-info__legend-color_free');
-    
-            if (seat.classList.contains('hall-info__legend-color_free')){
-                hallMapArr[hallRowNum-1][hallSeatNum-1].state = 'free';
-            }
-
-            if (seat.classList.contains('hall-info__legend-color_selected')){
-                hallMapArr[hallRowNum-1][hallSeatNum-1].state = 'selected';
-            }
-
-            showSelectedSeats(hallRowNum, hallSeatNum);
-            countSeatsPrice();
-
-            // console.log(hallMapArr);
-            console.log(selectedSeatsArr)
-        })
+        seat.addEventListener("click", seatCallback(seat));
     }
 }
+
+function seatCallback(place) {
+    const cb = function(event) {
+        if (place.classList.contains('hall-info__legend-color_taken')) {
+            place.removeEventListener("click", cb);
+            return;
+        }
+        seatEvent(place);
+    }
+    return cb;
+}
+
+function seatEvent(place) {
+    let hallRowNum = place.getAttribute('data-row');
+    let hallSeatNum = place.getAttribute('data-seat');
+
+    place.classList.toggle('hall-info__legend-color_selected');
+    place.classList.toggle('hall-info__legend-color_free');
+
+    if (place.classList.contains('hall-info__legend-color_free')){
+        hallMapArr[hallRowNum-1][hallSeatNum-1].state = 'free';
+    }
+
+    if (place.classList.contains('hall-info__legend-color_selected')){
+        hallMapArr[hallRowNum-1][hallSeatNum-1].state = 'selected';
+    }
+
+    showSelectedSeats(hallRowNum, hallSeatNum);
+    countSeatsPrice();
+}
+
 
 function showSelectedSeats(hallRowNum, hallSeatNum) {
     // show or hide the seat, user clicked, in the "selected seats" field at left
 
 
     let selectedSeat = document.querySelector('.film-info__seat_js');
-    let selectedSeatContainer = document.querySelector('.film-info__seat');
 
     selectedSeatsArr.push(hallMapArr[hallRowNum-1][hallSeatNum-1]);
-    selectedSeatsArr = selectedSeatsArr.filter(element => element.state == 'selected')
+    selectedSeatsArr = selectedSeatsArr.filter(element => element.state == 'selected');
 
     selectedSeatContainer.innerHTML = '';
 
@@ -83,12 +95,11 @@ function showSelectedSeats(hallRowNum, hallSeatNum) {
         selectedSeatContainer.innerHTML += selectedSeat.innerHTML;
         
     }
-    // console.log(selectedSeatContainer);
 
 }
 
 function countSeatsPrice() {
-    let totalCost = 0;
+    totalCost = 0;
 
     let finalCost = document.querySelector('.film-info__cost');
 
@@ -96,11 +107,40 @@ function countSeatsPrice() {
         totalCost += +seat.price;
     }
 
+    if (selectedSeatsArr.length == 0) {
+        totalCost = 0;
+    }
+
     finalCost.innerHTML = `${totalCost} $`
+}
+
+function buySelectedSeats() {
+    let button = document.querySelector('.film-info__button');
+
+    button.addEventListener('click', function() {
+        for (let seat of selectedSeatsArr) {
+            seat.state = 'taken';
+
+            for (let seat of seats) {
+                if (seat.classList.contains('hall-info__legend-color_selected'))
+                {
+                    seat.classList.remove('hall-info__legend-color_selected');
+                    seat.classList.add('hall-info__legend-color_taken');
+                }
+            }
+            
+        }
+        
+        selectedSeatContainer.innerHTML = '';
+        selectedSeatsArr = selectedSeatsArr.filter(element => element.state == 'selected');
+        countSeatsPrice();      
+    })
+    
 }
 
 
 fillHall();
 addSeatsToBuy();
+buySelectedSeats();
 
 
